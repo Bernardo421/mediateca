c888fa49d3691b3d1d0e95e195f055241d954250
Bernardo421 <bernardobernardofdala@gmail.com>
2026-08-09 23:17:31 +0100

    organização

--- js/animacao_do_login.js
+++ js/animacao_do_login.js
@@ -4,9 +4,9 @@ const mudar = document.getElementById("mudar_senha")
 mudar.addEventListener("click", () => {
     if(senha.type === "password"){
         senha.type = ("text")
-        mudar.src = "../image/olho.png";
+        mudar.src = "../login/image/olho.png";
     }else{
         senha.type = "password"
-        mudar.src = "../image/olho (1).png"
+        mudar.src = "../login/image/olho (1).png"
     }
 });
